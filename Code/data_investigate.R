579d36ef8af7cca5e9c5d9b93165055a382caee9
d34no98 <o.dean2@lancaster.ac.uk>
2025-11-18 23:56:10 +0000

    Analysis in to different splits of the data, Analysis on all data, and outliers separately. On comorbidity, chronic conditions, age, attendance type, ward type etc.

--- Code/data_investigate.R
+++ Code/data_investigate.R
@@ -1,12 +1,14 @@
+library(dplyr)
 library(readr)
 bed_data <- read_csv("./Code/WWL_data.csv")
 
 summary(bed_data)
+
+## Find some NA columns and exclude them from the dataset
 na_columns <- names(which(colSums(is.na(bed_data)) > 0))
 all_na_columns <- c()
 
 for (col in na_columns) {
-  browser()
   if (all(is.na(bed_data[col]))) {
     all_na_columns <- c(all_na_columns, col)
   }
@@ -15,20 +17,17 @@ for (col in na_columns) {
 bed_data_drop_na <- bed_data %>% 
   select(-all_na_columns)
 
-## Drop date variables
-bed_data_chars_numeric <- bed_data_drop_na %>% 
-  select(-date_columns)
-
+######## Analysis on the Full range of data ########
 ## Store all character and numeric variables separately
 library(stringr)
 char_columns <- 
-  names(bed_data_chars_numeric)[sapply(bed_data_chars_numeric, is.character)]
+  names(bed_data_drop_na)[sapply(bed_data_drop_na, is.character)]
 numeric_columns <- 
-  names(bed_data_chars_numeric)[sapply(bed_data_chars_numeric, is.numeric)]
+  names(bed_data_drop_na)[sapply(bed_data_drop_na, is.numeric)]
 
 for (col in char_columns) {
  print(
-   bed_data_chars_numeric %>%
+   bed_data_drop_na %>%
      group_by(!!sym(col)) %>%
      summarize(N=n())
  )
@@ -45,12 +44,12 @@ for (col in na_columns) {
 
 ## Investigate all Comorbidities against patient stay
 comorbid_columns <- 
-  names(bed_data_chars_numeric)[sapply(names(bed_data_chars_numeric), 
+  names(bed_data_drop_na)[sapply(names(bed_data_drop_na), 
                                        function(x) endsWith(x, "_flag") & startsWith(x, "comorbidity_"))]
 
 ## Investigate all Chronic conditions against patient stay
 chronic_columns <- 
-  names(bed_data_chars_numeric)[sapply(names(bed_data_chars_numeric), 
+  names(bed_data_drop_na)[sapply(names(bed_data_drop_na), 
                                        function(x) endsWith(x, "_flag") & startsWith(x, "chronic_"))]
 
 ## Derive length of stay using admission and discharge dates
@@ -60,8 +59,12 @@ bed_data_derive_dates <- bed_data %>%
          Date_of_admission = as.Date(admission_obj),
          discharge_obj = dmy_hms(discharge_date_dt, tz = "Europe/London"),
          Date_of_discharge = as.Date(discharge_obj),
-         duration_of_stay = (Date_of_discharge - Date_of_admission)) %>% 
+         duration_of_stay = (Date_of_discharge - Date_of_admission) + 1) %>% 
   select(-admission_obj, -discharge_obj)
+
+bed_data_derive_dates %>% 
+  filter(duration_of_stay >= 123) %>% 
+  select(duration_of_stay, spell_episode_los, admission_date_dt, discharge_date_dt)
 
 ## Summarize our derivation of length of stay against `spell_episode_los`
 summary(bed_data_derive_dates$duration_of_stay)
@@ -74,14 +77,23 @@ summary(bed_data$spell_episode_los) # Length of stay calculated in the original 
 # longer stays
 ####
 
-upper_25 <- bed_data_derive_dates %>% 
-  filter(duration_of_stay > quantile(duration_of_stay, 0.75, na.rm=TRUE))
+
+
+
+######## Investigate only on the Top 20% of LOS duration rows ########
+upper_20 <- bed_data_derive_dates %>% 
+  filter(duration_of_stay > quantile(duration_of_stay, 0.8, na.rm=TRUE))
+
+
+## Quintile summary of duration_of_stay 
+quintile_summary <- quantile(bed_data_derive_dates$duration_of_stay, probs = seq(0, 1, 0.01), na.rm = TRUE)
+print(quintile_summary)
 
 ## Plot box plots for each comorbidity flags
 library(ggplot2)
 for (col in comorbid_columns) {
   print(
-    ggplot(data=upper_25, aes(x=as.factor(!!sym(col)), y=duration_of_stay)) +
+    ggplot(data=upper_20, aes(x=as.factor(!!sym(col)), y=duration_of_stay)) +
       geom_boxplot()
   )
 }
@@ -92,11 +104,85 @@ for (col in comorbid_columns) {
 ## Plot box plots for each chronic condition flags
 for (col in chronic_columns) {
   print(
-    ggplot(data=upper_25, aes(x=as.factor(!!sym(col)), y=duration_of_stay)) +
+    ggplot(data=upper_20, aes(x=as.factor(!!sym(col)), y=duration_of_stay)) +
       geom_boxplot()
   )
 }
 
+## Look in to frailty score
+ggplot(data=upper_20, aes(x=frailty_score, y=duration_of_stay)) +
+  geom_boxplot() +
+  theme(axis.text.x = element_text(angle=45))
+### Results: same categories need to be placed with the same result
+### use `mutate to bring these categories together`
+library(stringr)
+upper_20_frailty_grouped <- upper_20 %>% 
+  mutate(dev_frailty_score = case_when(str_detect(frailty_score, "1") ~ "1 - Very Fit",
+                   str_detect(frailty_score, "2") ~ "2 - Well",
+                   str_detect(frailty_score, "3") ~ "3 - Managing Well",
+                   str_detect(frailty_score, "4") ~ "4 - Vulnerable",
+                   str_detect(frailty_score, "5") ~ "5 - Mildly Frail",
+                   str_detect(frailty_score, "6") ~ "6 - Moderately Frail",
+                   str_detect(frailty_score, "7") ~ "7 - Severely Frail",
+                   str_detect(frailty_score, "8") ~ "8 - Very Severely Frail",
+                   str_detect(frailty_score, "9") ~ "9 - Terminally Ill"))
 
-colnames(bed_data)
+## Boxplot on re-derived Frailty Score
+ggplot(data=upper_20_frailty_grouped, aes(x=dev_frailty_score, y=duration_of_stay)) +
+  geom_boxplot() +
+  theme(axis.text.x = element_text(angle=45))
+### RESULTS: No correlation found
 
+## Boxplot for Speciality departments length of stay for the Top 20%
+## of observations recording the longest lengths of stays
+ggplot(data=upper_20, aes(x=specialty_spec_desc, y=duration_of_stay)) +
+  geom_boxplot() +
+  coord_flip() +
+  labs(title="Speciality departments by length of Patient Stay",
+       subtitle = "Top 20% data")
+
+
+
+######## High associations spotted in High LOS outliers only ########
+
+## Analysis on the outliers of the data only
+
+outliers <- bed_data_derive_dates %>%
+  filter(duration_of_stay < quantile(duration_of_stay, 0.25, na.rm = TRUE) - 1.5 * IQR(duration_of_stay, na.rm = TRUE) |
+           duration_of_stay > quantile(duration_of_stay, 0.75, na.rm = TRUE) + 1.5 * IQR(duration_of_stay, na.rm = TRUE)) 
+
+
+
+## Plot box plots for each comorbidity flags
+for (col in comorbid_columns) {
+  print(
+    ggplot(data=outliers, aes(x=as.factor(!!sym(col)), y=duration_of_stay)) +
+      geom_boxplot()
+  )
+}
+
+## Plot box plots for each chronic condition flags
+for (col in chronic_columns) {
+  print(
+    ggplot(data=outliers, aes(x=as.factor(!!sym(col)), y=duration_of_stay)) +
+      geom_boxplot()
+  )
+}
+
+table(outliers$sex_description.y, useNA = "always")
+# We lose a 3rd of outlier data due to sex not being recorded
+
+ggplot(data=outliers, aes(x=patient_age_on_admission, y=duration_of_stay,
+                          col = sex_description.y)) +
+  geom_point()
+
+ggplot(data=outliers, aes(x=ward_type_admission, y=duration_of_stay)) +
+  geom_boxplot() +
+  coord_flip()
+
+ggplot(data=outliers, aes(x=ward_type_admission, y=duration_of_stay)) +
+  geom_boxplot()
+
+ggplot(data=outliers, aes(x=patient_age_on_discharge, y=duration_of_stay,
+                          col = sex_description.y)) +
+  geom_point()
