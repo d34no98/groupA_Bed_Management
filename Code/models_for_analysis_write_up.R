f80bf17328fcdf1376396570f75085ccfa22345e
d34no98 <o.dean2@lancaster.ac.uk>
2025-12-01 21:00:54 +0000

    New code to place R2 stats in the footnote. Bold any significant P-values, show all diagnostic stats (AIC)

--- Code/models_for_analysis_write_up.R
+++ Code/models_for_analysis_write_up.R
@@ -88,6 +88,9 @@ library(performance)
 library(glue)
 r2_log_vals <- performance::r2(log_model_7)
 
+#install.packages("parameters")
+library(parameters)
+
 ## Fixed Effects Table
 log_model_summary <- tbl_regression(
   log_model_7,
@@ -100,14 +103,15 @@ log_model_summary <- tbl_regression(
   intercept = TRUE,
   estimate_fun = function(x) style_number(x, digits = 3),
   pvalue_fun = label_style_pvalue(digits=3)) %>% 
-  bold_labels() %>% 
-  add_glance_source_note() %>% 
+  bold_p(t=0.05) %>% 
+  add_glance_source_note() %>%
   modify_table_styling(
     columns = label,
     footnote = glue::glue(
       "Model fit: Marginal R² = {round(r2_log_vals$R2_marginal, 3)}, Conditional R² = {round(r2_log_vals$R2_conditional, 3)}"
     )
-  )
+  ) %>% 
+  bold_labels()
 
 ## Export the final model
 library(flextable)
@@ -203,6 +207,8 @@ library(glue)
 r2_nbinom_vals <- performance::r2(nbinom_model_7)
 
 ## Write the model in a FlexTable format
+#install.packages("parameters")
+library(parameters)
 nbinom_model_summary <- tbl_regression(
   nbinom_model_7,
   label = list(patient_age_on_admission ~ "Age (Years) on Admission",
@@ -215,14 +221,17 @@ nbinom_model_summary <- tbl_regression(
   intercept = TRUE,
   estimate_fun = function(x) style_number(x, digits = 3),
   pvalue_fun = label_style_pvalue(digits=3)) %>% 
-  bold_labels() %>% 
-  add_glance_source_note() %>% 
+#  add_global_p() %>% 
+  bold_p(t=0.05) %>% 
+  add_glance_source_note() %>%
   modify_table_styling(
     columns = label,
     footnote = glue::glue(
       "Model fit: Marginal R² = {round(r2_nbinom_vals$R2_marginal, 3)}, Conditional R² = {round(r2_nbinom_vals$R2_conditional, 3)}"
     )
-  )
+  ) %>% 
+  bold_labels()
+  
 
 ## Export the final Negative Binomial Model
 library(flextable)
@@ -235,10 +244,3 @@ nbinom_model_summary %>%
   save_as_docx(
     path = "final_NB_regression_model_results.docx")
 
-library(broom.mixed)
-library(gtsummary)
-library(dplyr)
-
-library(performance)
-r.squaredGLMM(log_model_7) ## MuMIn Package
-performance::r2_nakagawa(log_model_7)
